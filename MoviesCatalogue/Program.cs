417d96fa264cd7364b9924be967267f7cdddd250
Josue Villeda <josuechacon46@gmail.com>
2023-04-05 00:38:56 -0600

    Added database conection and implement swager in Program file

--- MoviesCatalogue/Program.cs
+++ MoviesCatalogue/Program.cs
@@ -1,3 +1,6 @@
+using MoviesCatalogue.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Register database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,3 +34,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
