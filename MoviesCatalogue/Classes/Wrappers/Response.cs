2c437c84c11d7efb32678d6f941b67154c038287
Josue Villeda <josuechacon46@gmail.com>
2023-04-07 00:22:57 -0600

    Created Response class to manage errors and messages

---
+++ MoviesCatalogue/Classes/Wrappers/Response.cs
@@ -0,0 +1,32 @@
+﻿namespace MoviesCatalogue.Classes.Wrappers
+{
+    public class Response<T>
+    {
+        public Response()
+        {
+        }
+
+        public Response(string message, string error, T data) { 
+            Success = false;
+            Status = 400;
+            Message = message;
+            Data =  data;
+            Error = error;
+        }
+
+        public Response(string message, T data)
+        {
+            Success = true;
+            Status = 200;
+            Message = message;
+            Data = data;
+            Error = "";
+        }
+
+        public bool Success { get; set; }
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public string Error { get; set; }
+    }
+}
