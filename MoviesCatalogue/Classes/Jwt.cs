9bb94407ec3a5ced302da92a0af9e7bb5993b8e0
Josue Villeda <josuechacon46@gmail.com>
2023-04-07 06:11:44 -0600

    Created a method to Get Claim Id for the authenticated user in Jwt class

--- MoviesCatalogue/Classes/Jwt.cs
+++ MoviesCatalogue/Classes/Jwt.cs
@@ -1,8 +1,10 @@
-﻿using MoviesCatalogue.Context;
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesCatalogue.Context;
 using MoviesCatalogue.Models;
 using NuGet.Common;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Principal;
 
 namespace MoviesCatalogue.Classes
 {
@@ -12,6 +14,31 @@ namespace MoviesCatalogue.Classes
         public string Issuer { get; set; }
         public string Audience { get; set; }
         public string Subject { get; set; }
+
+        public static int GetClaimId(ClaimsIdentity identity)
+        {
+            int claimId = 0;
+
+            try
+            {
+                if (identity.Claims.Any())
+                {
+
+                    var Id = identity.Claims.FirstOrDefault(x => x.Type == "Id").Value;
+
+                    if (int.TryParse(Id, out int userId))
+                    {
+                        return userId;
+                    }
+                }
+
+                return claimId;
+            }
+            catch (Exception)
+            {
+                return claimId;
+            }
+        }
 
     }
 }
