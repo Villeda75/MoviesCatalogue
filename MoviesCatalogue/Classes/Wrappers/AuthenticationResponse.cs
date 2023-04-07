29622930bb279aeb2e4077c39ccd23eb9784a758
Josue Villeda <josuechacon46@gmail.com>
2023-04-07 01:46:07 -0600

    Created Authentication response class to manage messages, errors and token

---
+++ MoviesCatalogue/Classes/Wrappers/AuthenticationResponse.cs
@@ -0,0 +1,30 @@
+﻿using NuGet.Common;
+using System.Drawing.Printing;
+
+namespace MoviesCatalogue.Classes.Wrappers
+{
+    public class AuthenticationResponse<T> : Response<T>
+    {
+        public AuthenticationResponse(string message, string error, T data, T token)
+        {
+            Success = false;
+            Status = 400;
+            Message = message;
+            Token = token;
+            Data = data;
+            Error = error;
+        }
+
+        public AuthenticationResponse(string message, T data, T token)
+        {
+            Success = true;
+            Status = 200;
+            Message = message;
+            Token = token;
+            Data = data;
+            Error = "";
+        }
+
+        public T Token { get; set; }
+    }
+}
