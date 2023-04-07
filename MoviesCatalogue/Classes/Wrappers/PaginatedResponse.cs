52c5519714f77643d54fb3f0c3a68df8c159ee8b
Josue Villeda <josuechacon46@gmail.com>
2023-04-07 00:25:50 -0600

    Created a PaginatedResponse class for paginated movie list

---
+++ MoviesCatalogue/Classes/Wrappers/PaginatedResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace MoviesCatalogue.Classes.Wrappers
+{
+    public class PaginatedResponse<T> : Response<T>
+    {
+            public PaginatedResponse(T data, int pageNumber, int pageSize, int totalPages, int totalRecords)
+            {
+
+                PageNumber = pageNumber;
+                PageSize = pageSize;
+                TotalPages = totalPages;
+                TotalRecords = totalRecords;
+                Success = true;
+                Status = 200;
+                Message = "";
+                Data = data;
+                Error = "";
+
+            }
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+    }
+}
