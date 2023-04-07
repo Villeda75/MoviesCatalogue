dbdcae295be3e24d1a772376e2f12204278392cd
Josue Villeda <josuechacon46@gmail.com>
2023-04-07 15:14:11 -0600

    Created new wrapper for Create movies

---
+++ MoviesCatalogue/Classes/Wrappers/CreateMovie.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Framework;
+
+namespace MoviesCatalogue.Classes.Wrappers
+{
+    public class CreateMovie
+    {
+        [Required]
+        public string Name { get; set; }
+        public int ReleaseYear { get; set; }
+        [Required]
+        public string Synopsis { get; set; }
+        public string? ImagePoster { get; set; }
+        [Required]
+        public string Category { get; set; }
+    }
+}
