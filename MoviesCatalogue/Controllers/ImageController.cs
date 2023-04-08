737103510c0129ea023c71233c6e3a022fa89a52
Josue Villeda <josuechacon46@gmail.com>
2023-04-07 22:15:20 -0600

    Created new controller to allow user to upload images

---
+++ MoviesCatalogue/Controllers/ImageController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MoviesCatalogue.Controllers
+{
+    [Route("images")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        [HttpPost]
+        [RequestSizeLimit(100_000_000)] // 100 MB limit
+        public async Task<IActionResult> Upload([FromForm] IFormFile file)
+        {
+            try
+            {
+                if (file == null || file.Length == 0)
+                    return BadRequest("Please select a file");
+
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Resources", "Uploads", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                return Ok(new { fileName });
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+
+    }
+}
