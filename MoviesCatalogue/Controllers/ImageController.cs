a79a8fb28015451c61af6807ddd38fbf8cb7ec5e
Josue Villeda <josuechacon46@gmail.com>
2023-04-07 23:55:10 -0600

    Completed Upload image to movie Added Get movie method

--- MoviesCatalogue/Controllers/ImageController.cs
+++ MoviesCatalogue/Controllers/ImageController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
+using MoviesCatalogue.Context;
 
 namespace MoviesCatalogue.Controllers
 {
@@ -9,30 +11,95 @@ namespace MoviesCatalogue.Controllers
     [ApiController]
     public class ImageController : ControllerBase
     {
-        [HttpPost]
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _environment;
+
+        public ImageController(AppDbContext context, IWebHostEnvironment environment) { 
+            _context = context;
+            _environment = environment;
+        }
+
+        [Authorize(Policy = "AdminPermission")]
+        [HttpPost("{id}")]
         [RequestSizeLimit(100_000_000)] // 100 MB limit
-        public async Task<IActionResult> Upload([FromForm] IFormFile file)
+        public async Task<IActionResult> Upload(int id, [FromForm] IFormFile file)
         {
             try
             {
-                if (file == null || file.Length == 0)
-                    return BadRequest("Please select a file");
-
-                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Resources", "Uploads", fileName);
-
-                using (var stream = new FileStream(filePath, FileMode.Create))
-                {
-                    await file.CopyToAsync(stream);
+                if (id == 0) {
+                    return BadRequest("Movie Id is empty.");
                 }
 
-                return Ok(new { fileName });
+                if (file == null || file.Length == 0)
+                    return BadRequest("File is empty.");
+
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                if (SaveFile(file, fileName).Result)
+                {
+                    var movie = await _context.Movies.FindAsync(id);
+
+                    if (movie is null)
+                    {
+                        return NotFound("Movie not found.");
+                    }
+
+                    movie.ImagePoster = fileName;
+
+                    _context.Entry(movie).State = EntityState.Modified;
+
+                    try
+                    {
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        return BadRequest("Could not save image.");
+                    }
+
+                    return Ok(new { fileName });
+                }
+
+                return BadRequest("File could not saved.");
             }
             catch (Exception error)
             {
                 return BadRequest(error.Message);
             }
         }
+
+        [HttpGet("{name}")]
+        public IActionResult ObtenerImagen(string name)
+        {
+            var pathImagen = Path.Combine(Directory.GetCurrentDirectory(), "Resources", "Uploads", name);
+
+            if (!System.IO.File.Exists(pathImagen))
+            {
+                return NotFound();
+            }
+
+            var imagenStream = new FileStream(pathImagen, FileMode.Open, FileAccess.Read);
+            return new FileStreamResult(imagenStream, "image/jpeg");
+        }
+
+        private static async Task<bool> SaveFile(IFormFile file, string name)
+        {
+            try
+            {
+                var fileName = name;
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Resources", "Uploads", fileName);
+
+                using var stream = new FileStream(filePath, FileMode.Create);
+                await file.CopyToAsync(stream);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
 
     }
 }
